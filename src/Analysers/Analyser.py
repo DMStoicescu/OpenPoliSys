affa6743f1db07168fd0b7e1e53904fcbb6a7648
DMStoicescu <danmihaistoicescu@hotmail.com>
2025-07-10 01:04:07 +0300

    Add deepseek pipeline and append the master prompt and privacy policy to all pipelines.

--- src/Analysers/Analyser.py
+++ src/Analysers/Analyser.py
@@ -4,10 +4,11 @@ from openai import OpenAI
 
 class Analyser:
 
-    master_prompt = ['Just say "hello world" to anything I tell you']
+    master_prompt = 'Just say "hello world" to anything I tell you'
 
     anthropic_model = "claude-3-sonnet-latest"
     openai_model = "gpt-4o"
+    deepseek_model = "deepseek-reasoner"
 
     def __init__(self, privacy_policy, model = "OpenAI"):
         # Set the privacy policy text
@@ -29,6 +30,11 @@ class Analyser:
             # Send for analysis
             self.analyse_privacy_policy_Anthropic()
 
+        elif self.model == "DeepSeek":
+            # Get DeepSeek API
+            self.api_key = os.getenv("DEEPSEEK_API_KEY")
+            # Send for analysis
+            self.analyse_privacy_policy_DeepSeek()
 
     def analyse_privacy_policy_OpenAI(self):
 
@@ -45,8 +51,7 @@ class Analyser:
             temperature = 0,
             messages = [
                 {"role": "developer", "content": self.master_prompt},
-                {"role": "user", "content": "How do I check if a Python object is an instance of a class?",
-                },
+                {"role": "user", "content": self.privacy_policy},
             ],
         )
 
@@ -64,14 +69,14 @@ class Analyser:
             model = self.anthropic_model,
             max_tokens =  20000,
             temperature = 0,
-            system = "You are a world-class poet. Respond only with short poems.",
+            system = self.master_prompt,
             messages = [
                 {
                     "role": "user",
                     "content": [
                         {
                             "type": "text",
-                            "text": "Why is the ocean salty?"
+                            "text": self.privacy_policy,
                         }
                     ]
                 }
@@ -82,11 +87,29 @@ class Analyser:
 
         return reply
 
+    def analyse_privacy_policy_DeepSeek(self):
+        # Set the reply to default empty string
+        reply = ''
+
+        # Set the api_key
+        client = OpenAI(api_key=self.api_key, base_url="https://api.deepseek.com")
+
+        # Send prompt and create response
+        response = client.chat.completions.create(
+            model = self.deepseek_model,
+            messages = [
+                {"role": "system", "content": self.master_prompt},
+                {"role": "user", "content": self.privacy_policy},
+            ],
+            stream=False
+        )
+
 
 # Test code
 # TODO: REMOVE
 # OpenAI
 # analyser = Analyser(privacy_policy="This is a privacy policy", model="OpenAI")
 # Anthropic
-analyser = Analyser(privacy_policy="This is a privacy policy", model="Anthropic")
-
+# analyser = Analyser(privacy_policy="This is a privacy policy", model="Anthropic")
+# DeepSeek (interestingly works with OpenAI API)
+analyser = Analyser(privacy_policy="This is a privacy policy", model="DeepSeek")
